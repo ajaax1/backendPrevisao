b5d39893a7d31b9864b2eead8afff56f1295b363
ajaax1 <ruanhigor123@gmail.com>
2024-07-31 19:44:48 -0300

    final

--- app/Http/Controllers/ApiTempo.php
+++ app/Http/Controllers/ApiTempo.php
@@ -11,14 +11,15 @@ class ApiTempo extends Controller
      */
     public function api($cidade)
     {
-        $responseGeo = Http::get("http://api.openweathermap.org/geo/1.0/direct?q=$cidade&appid=0a06f95727a0a65a8165050db073611c");
+        $apiKey = env('API_KEY');
+        $responseGeo = Http::get("http://api.openweathermap.org/geo/1.0/direct?q=$cidade&appid=$apiKey");
 
         if ($responseGeo->successful()) {
             $geoData = $responseGeo->json();
             $lat = $geoData[0]['lat'];
             $lon = $geoData[0]['lon'];
 
-            $responseWeather = Http::get("https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=0a06f95727a0a65a8165050db073611c&units=metric&lang=pt_br");
+            $responseWeather = Http::get("https://api.openweathermap.org/data/2.5/forecast?lat=$lat&lon=$lon&appid=$apiKey&units=metric&lang=pt_br");
 
             if ($responseWeather->successful()) {
                 $weatherData = $responseWeather->json();
